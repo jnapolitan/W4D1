00456c665da044d91285d2e77e641dd041c6e838
jnapolitan <julian.napolitan@gmail.com>
2018-11-12 15:30:06 -0800

    Add Artworks and Users controllers

---
+++ route_project/app/models/artwork.rb
@@ -0,0 +1,29 @@
+# == Schema Information
+#
+# Table name: artworks
+#
+#  id         :bigint(8)        not null, primary key
+#  title      :string           not null
+#  image_url  :string           not null
+#  artist_id  :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Artwork < ApplicationRecord
+  validates :title, uniqueness: { scope: :artist_id,
+    message: "artist cannot have two pieces with the same title" }
+  validates :title, :artist_id, :image_url, presence: true
+  
+  belongs_to :user,
+    foreign_key: :artist_id,
+    class_name: "User"
+    
+  has_many :artwork_shares,
+    foreign_key: :artwork_id,
+    class_name: "ArtworkShare"
+    
+  has_many :shared_viewers,
+    through: :artwork_shares,
+    source: :viewer
+end
--- route_project/db/seeds.rb
+++ route_project/db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+  u1 = User.create(username: "eric")
+  u2 = User.create(username: "julian")
+  u3 = User.create(username: "emma")
+  u4 = User.create(username: "sue")
+  u5 = User.create(username: "ryan")
+ 
+Artwork.destroy_all
+  a1 = Artwork.create(title: "My masterpiece", image_url: 'http://www.image.com', artist_id: u1.id)
+  a2 = Artwork.create(title: "My masterpiece 2", image_url: 'http://www.image2.com', artist_id: u1.id)
+  a3 = Artwork.create(title: "My masterpiece 3", image_url: 'http://www.image3.com', artist_id: u1.id)
+  a4 = Artwork.create(title: "Picasso cover", image_url: 'http://www.picasso.com', artist_id: u2.id)
+  a5 = Artwork.create(title: "Picasso cover 2", image_url: 'http://www.picasso2.com', artist_id: u2.id)
+  
+ArtworkShare.destroy_all
+  as1 = ArtworkShare.create(artwork_id: a1.id, viewer_id: u5.id)
+  as2 = ArtworkShare.create(artwork_id: a1.id, viewer_id: u4.id)
+  as3 = ArtworkShare.create(artwork_id: a4.id, viewer_id: u3.id)
+  as4 = ArtworkShare.create(artwork_id: a3.id, viewer_id: u3.id)
+  as5 = ArtworkShare.create(artwork_id: a2.id, viewer_id: u2.id)
+  
+
